5dbb67e8a5e1944090f0f1d748b48c2edb27164f
Carla Wilby <carla@zelda.org.za>
2018-10-14 12:52:37 +0200

    Resolved DocumentSnapshot value calls

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-var Message = require("./message.js");
-var pythonInit = require("./nodeInitiatePythonScript.js");
+const functions = require("firebase-functions")
+let Message = require("./message.js");
+const pythonInit = require("./nodeInitiatePythonScript.js");
 
 exports.triggerNewMessageWrite = functions.firestore
     .document('{userCollectionId}/{messageId}')
     .onCreate((snap, context) => {
-        // TODO: get user id from snap?
-        const userId = snap.name();
+        const userId = snap.id;
         const newMessage = snap.data();
         const messageFromBot = newMessage.messageFromBot;
         const messageSendTime = newMessage.messageSendTime;
         const messageText = newMessage.messageText;
 
-        if(messageFromBot === False) {
+        if(messageFromBot === false) {
             // new message object
-            var userMessage = new Message(messageFromBot, messageSendTime, messageText);
+            let userMessage = new Message(messageFromBot, messageSendTime, messageText);
             pythonInit(userId, messageText);
             console.log("Initiating Message transfer to APIConnect");
         }else {
