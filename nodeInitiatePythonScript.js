d2b939224a79175a18c168158583087e39aed585
Carla Wilby <carla@zelda.org.za>
2018-10-14 12:22:31 +0200

    Refactoring

--- nodeInitiatePythonScript.js
+++ nodeInitiatePythonScript.js
@@ -4,12 +4,12 @@ var submitResponse = require("./submitResponse.js");
 
 module.exports = function(userId, messageText) {
     // Spawn Python script with Message as argument
-    const pythonProcess = spawn('python',["controller.py"], userId, messageText);
+    const pythonProcess = spawn('python',["./APIConnect/controller.py"], userId, messageText);
     pythonProcess.stdout.on('data', (data) => {
         if(data.startsWith("Data:")){
             // Remove message header
             let response = data.split(':')[1].trim();
-            var responseMessage = new Message(messageText);
+            var responseMessage = new Message(response);
             submitResponse(userId, responseMessage);
         }
     });
